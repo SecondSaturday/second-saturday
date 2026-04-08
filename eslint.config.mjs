8865581c345aa74c601f118cf3485ee5bc8030ab
CalyanV <calyanv12@outlook.com>
2026-04-08 01:49:29 -0400

    chore: fix dependency placement, remove no-op workspaces, wire up prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -16,6 +17,7 @@ const eslintConfig = defineConfig([
     "ios/**",
     "android/**",
   ]),
+  prettier,
 ]);
 
 export default eslintConfig;
